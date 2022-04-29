88e7e92fc8b496d449477aa837a7bbf7e883813b
Kou Matsumoto <develop@kou-matsumoto.jp>
2022-04-29 12:16:24 +0900

    fix: use globalThis instead window for service worker

--- src/environments.ts
+++ src/environments.ts
@@ -1,9 +1,9 @@
 const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
+  globalThis.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
+    globalThis.location.hostname === '[::1]' ||
     // 127.0.0.0/8 are considered localhost for IPv4.
-    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/),
+    globalThis.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/),
 );
 const isProduction = process.env.NODE_ENV === 'production';
 const publicURL = process.env.PUBLIC_URL;
