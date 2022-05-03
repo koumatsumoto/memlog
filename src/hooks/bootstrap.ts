4cc75c617d6472a5e2365dec8d4b073e7878ddd0
Kou Matsumoto <develop@kou-matsumoto.jp>
2022-05-03 22:13:15 +0900

    feat: show dialog if title and text exists in url params

--- src/hooks/bootstrap.ts
+++ src/hooks/bootstrap.ts
@@ -1,17 +1,27 @@
 import { useEffect } from 'react';
+import { atom, useRecoilValue } from 'recoil';
+import { getUrlQueryParams } from '../utils';
 import { replaceLocationWithTopPage, requestAccessTokenAndSaveToStorage, useGitHubAccessToken, useGitHubAuthCode } from './auth';
 
+const initialUrlParamsState = atom({ key: 'initialUrlParamsState', default: getUrlQueryParams() });
+
 export const useApplicationBootstrap = () => {
+  const urlParams = useRecoilValue(initialUrlParamsState);
   const githubAuthCode = useGitHubAuthCode();
   const githubAccessToken = useGitHubAccessToken();
   const loginStatus = githubAccessToken ? 'LoggedIn' : githubAuthCode ? 'GettingAccessToken' : 'NotLoggedIn';
 
   useEffect(() => {
-    if (githubAuthCode) {
-      requestAccessTokenAndSaveToStorage(githubAuthCode).then(replaceLocationWithTopPage);
+    // page redirected from github login page for oauth
+    if (urlParams.code) {
+      requestAccessTokenAndSaveToStorage(urlParams.code).then(replaceLocationWithTopPage);
+    }
+    // page opened by Web Share API
+    if (urlParams.title && urlParams.text) {
+      alert(`Page opened by Web Share API, title=${urlParams.title}, text=${urlParams.text}`);
     }
 
-    console.log('[info] application bootstrap', { loginStatus, githubAuthCode, githubAccessToken });
+    console.log('[info] application bootstrap', { urlParams, loginStatus });
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return {
